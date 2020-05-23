eb9c2605b494f9dad80ad9813a6c4dced12bc485
viktorstrate <viktorstrate@gmail.com>
2020-05-23 21:54:03 +0200

    Initial Commit

---
+++ SwiftTreeSitter/SwiftTreeSitter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTreeSitter.h
+//  SwiftTreeSitter
+//
+//  Created by Viktor Strate Kløvedal on 23/05/2020.
+//  Copyright © 2020 viktorstrate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftTreeSitter.
+FOUNDATION_EXPORT double SwiftTreeSitterVersionNumber;
+
+//! Project version string for SwiftTreeSitter.
+FOUNDATION_EXPORT const unsigned char SwiftTreeSitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTreeSitter/PublicHeader.h>
+
+
