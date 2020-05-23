cc35b20cba013f47d0236661b5df61392d4cbb3e
viktorstrate <viktorstrate@gmail.com>
2020-05-23 21:58:07 +0200

    Tests works

---
+++ SwiftTreeSitter/Parser.swift
@@ -0,0 +1,17 @@
+//
+//  Parser.swift
+//  SwiftTreeSitter
+//
+//  Created by Viktor Strate Kløvedal on 23/05/2020.
+//  Copyright © 2020 viktorstrate. All rights reserved.
+//
+
+import Foundation
+import SwiftTreeSitter.CTreeSitter
+
+class STSParser {
+    
+    init() {
+        ts_parser_new()
+    }
+}
